6fea67e178dec9ae53703b94282588f0176540f7
georgyccny <georgy.orlov@mssm.edu>
2023-12-01 15:41:30 -0500

    Add user login/logout etc

--- api/models/product.py
+++ api/models/product.py
@@ -2,61 +2,65 @@ import psycopg2
 from flask import Blueprint
 from database import conn
 
+def create_table():
+    # Connection parameters
+
+    # Create a cursor object to execute SQL queries
+    cursor = conn.cursor()
+
+    # SQL statement to create a 'products' table
+    create_table_query = '''
+        CREATE TABLE IF NOT EXISTS products (
+            product_id SERIAL PRIMARY KEY,
+            name VARCHAR(255),
+            type VARCHAR(255),
+            price NUMERIC,
+            model VARCHAR(255),
+            size VARCHAR(255),
+            color VARCHAR(255),
+            description TEXT
+        )
+    '''
+
+    try:
+        # Execute the SQL query to create the table
+        cursor.execute(create_table_query)
+        conn.commit()
+        return 'Table created successfully'
+    except Exception as e:
+        conn.rollback()
+        return f'Error: {e}'
+    finally:
+        # Close cursor and connection
+        cursor.close()
+
+
+def drop_table():
+    cursor = conn.cursor()
+
+    drop_table_query = f'''
+    DROP TABLE IF EXISTS products
+    '''
+
+    try:
+        cursor.execute(drop_table_query)
+        conn.commit()
+        return f'Table products dropped successfully'
+    except Exception as e:
+        conn.rollback()
+        return f'Error dropping products table: {e}'
+    finally:
+        cursor.close()
+
 
 class Product:
-    def __init__(self):
-        self.table_name = 'products'
+    def __init__(self, name='', price=0, model = '', size = '', color = '', description = ''):
+        self.product_id = None
+        self.name = name
+        self.price = price
+        self.model = model
+        self.size = size
+        self.color = color
+        self.description = description
 
-    def create_table(self):
-        # Connection parameters
-
-        # Create a cursor object to execute SQL queries
-        cursor = conn.cursor()
-
-        # SQL statement to create a 'products' table
-        create_table_query = '''
-            CREATE TABLE IF NOT EXISTS products (
-                product_id SERIAL PRIMARY KEY,
-                name VARCHAR(255),
-                type VARCHAR(255),
-                price NUMERIC,
-                model VARCHAR(255),
-                size VARCHAR(255),
-                color VARCHAR(255),
-                description TEXT
-            )
-        '''
-
-        try:
-            # Execute the SQL query to create the table
-            cursor.execute(create_table_query)
-            conn.commit()
-            return 'Table created successfully'
-        except Exception as e:
-            conn.rollback()
-            return f'Error: {e}'
-        finally:
-            # Close cursor and connection
-            cursor.close()
-
-
-    def drop_table(self):
-        cursor = conn.cursor()
-
-        drop_table_query = f'''
-        DROP TABLE IF EXISTS {self.table_name}
-        '''
-
-        try:
-            cursor.execute(drop_table_query)
-            conn.commit()
-            return f'Table {self.table_name} dropped successfully'
-        except Exception as e:
-            conn.rollback()
-            return f'Error dropping {self.table_name} table: {e}'
-        finally:
-            cursor.close()
-
-
-product = Product()
-product.create_table()
+create_table()
---
+++ api/models/user.py
@@ -0,0 +1,88 @@
+import psycopg2
+from flask import Blueprint
+from database import conn
+from flask_login import UserMixin
+
+def create_table():
+    # Connection parameters
+
+    # Create a cursor object to execute SQL queries
+    cursor = conn.cursor()
+
+    # SQL statement to create a 'users' table
+    create_table_query = '''
+        CREATE TABLE IF NOT EXISTS users (
+            user_id SERIAL PRIMARY KEY,
+            first_name VARCHAR(255),
+            last_name VARCHAR(255),
+            email VARCHAR(255),
+            password VARCHAR(255)
+        )
+    '''
+
+    try:
+        # Execute the SQL query to create the table
+        cursor.execute(create_table_query)
+        conn.commit()
+        return 'Table users created successfully'
+    except Exception as e:
+        conn.rollback()
+        return f'Error: {e}'
+    finally:
+        # Close cursor and connection
+        cursor.close()
+
+
+def drop_table(self):
+    cursor = conn.cursor()
+
+    drop_table_query = '''
+    DROP TABLE IF EXISTS users
+    '''
+
+    try:
+        cursor.execute(drop_table_query)
+        conn.commit()
+        return f'Table users dropped successfully'
+    except Exception as e:
+        conn.rollback()
+        return f'Error dropping users table: {e}'
+    finally:
+        cursor.close()
+
+
+class User(UserMixin):
+    def __init__(self, first_name='', last_name='', email='', password=''):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.email = email
+        self.password = password
+        self.user_id = None
+    
+
+    def add_user_to_db(self):
+        cursor = conn.cursor()
+        
+
+        insert_query = '''
+        INSERT INTO users (first_name, last_name, email, password)
+        VALUES (%s, %s, %s, %s) RETURNING user_id
+        '''
+        data = (self.first_name, self.last_name, self.email, self.password)
+
+        try:
+            cursor.execute(insert_query, data)
+            user_id = cursor.fetchone()[0]
+            conn.commit()
+            cursor.close()
+            self.user_id = user_id
+            return user_id
+        except Exception as e:
+            conn.rollback()
+            cursor.close()
+            raise e
+
+    def get_id(self):
+        return self.user_id
+
+create_table()
