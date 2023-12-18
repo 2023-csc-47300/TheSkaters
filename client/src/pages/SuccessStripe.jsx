a65e152db0238f9e2ec862f47c7ba5736b1c2ca2
georgyccny <georgy.orlov@mssm.edu>
2023-12-18 18:14:47 -0500

    Finish stripe frontend

---
+++ client/src/pages/SuccessStripe.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../App.css';
+
+const SuccessStripe = () => {
+    return (
+        <>
+        <h2>Payment was successful!</h2>
+        </>
+    )
+}
+
+export default SuccessStripe
