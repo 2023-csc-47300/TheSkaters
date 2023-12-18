1db996285861aa1fd9d93b8ac68721ceb4a222f3
YevheniyaSolomyana <72937387+YevheniyaSolomyana@users.noreply.github.com>
2023-12-18 18:15:42 -0500

    centering h2

---
+++ client/src/pages/SuccessStripe.jsx
@@ -0,0 +1,9 @@
+const SuccessStripe = () => {
+    return (
+        <>
+        <h2 style={{ textAlign: 'center', marginTop: '30px'}}>Payment was successful!</h2>
+        </>
+    )
+}
+
+export default SuccessStripe
