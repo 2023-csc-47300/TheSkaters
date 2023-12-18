21ddb0083cb419a666abc98cf407d1f9bbea8a73
georgyccny <georgy.orlov@mssm.edu>
2023-12-18 11:42:31 -0500

    Add signup

--- client/src/pages/LogIn.jsx
+++ client/src/pages/LogIn.jsx
@@ -1,11 +1,13 @@
 import React, { useState, useEffect } from 'react';
 import '../styles/Pages.css';
 import UserAPI from '../services/UserAPI';
+import { Link } from 'react-router-dom';
 
 const LogIn = () => {
 
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+    const [error, setError] = useState(null);
 
     const handleGitHubLogin = async () => {
         try {
@@ -24,7 +26,15 @@ const LogIn = () => {
           const response = await UserAPI.loginLocally(email, password); // Adjust with your local login API
           // Handle the response, possibly redirect the user or display data
           console.log(response);
-          window.location.href = '/';
+          if (response.error) {
+            console.log(response.error.description);
+            setError(response.error.description); // Set the error message to display
+            setTimeout(() => {
+                setError(null);
+            }, 5000); // Clear the error message after 5 seconds (adjust as needed)
+          } else {
+            window.location.href = '/';
+          }
       } catch (error) {
           console.error(error);
       }
@@ -36,6 +46,7 @@ const LogIn = () => {
           
           <form onSubmit={handleLocalLogin}>
                 <h2>Local Login</h2>
+                {error && <div className="error">{error}</div>}
                 <div>
                     <label>Email:</label>
                     <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
@@ -46,6 +57,7 @@ const LogIn = () => {
                 </div>
                 <button type="submit">Login</button>
           </form>
+          <Link to="/signUp"><button className='signUpBtn'>Sign Up</button></Link>
 
         </div>
       );
